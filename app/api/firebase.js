396221ab982734e5225232b92c86efbfc90bc953
uyencfi <77076051+uyencfi@users.noreply.github.com>
2021-06-12 17:57:41 +0800

    Set up Cloud Firestore database.
    Added databaseURL to firebaseConfig.
    Please update the dotenv file accordingly.

--- app/api/firebase.js
+++ app/api/firebase.js
@@ -1,23 +1,23 @@
 import firebase from "firebase/app"
 import "firebase/auth"
-import "firebase/database"
+import "firebase/firestore"
 
 import {
     API_KEY,
     AUTH_DOMAIN,
-    // DATABASE_URL,
+    DATABASE_URL,
     PROJECT_ID,
     STORAGE_BUCKET,
     MESSAGING_SENDER_ID,
     APP_ID,
     MEASUREMENT_ID
-  } from "@env";
+} from "@env";
 
 const firebaseConfig = {
-    //please setup .env file 
+    //please setup .env file
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
-    // databaseURL: DATABASE_URL,
+    databaseURL: DATABASE_URL,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDER_ID,
