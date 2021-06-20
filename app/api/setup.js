11bc32dd5e74370081758ab880b25805dd6930ca
uyencfi <77076051+uyencfi@users.noreply.github.com>
2021-06-20 21:43:07 +0800

    [Set up] Add feature to upload user profile picture.

--- app/api/setup.js
+++ app/api/setup.js
@@ -3,34 +3,47 @@ import firebase from "./firebase";
 const db = firebase.firestore();
 
 // push profile data to db
-export const setUpProfile = async ({age, level, place, date}, onSuccess, onError) => {
-
+export const setUpProfile = async ({age, level, place, date, avatar}, onSuccess, onError) => {
+  try {
     const user = firebase.auth().currentUser;
     if (user) {
       const uid = user.uid;
       // parse inputs
+      if (avatar == null) return onError("Please add a profile picture");
+      // const response = await fetch(avatar.uri);
+      // const blob = await response.blob();
+      const fileRef = firebase.storage().ref().child(uid + "/avatar/");
+      fileRef.put(avatar.uri);
+      const avatarUrl = await fileRef.getDownloadURL();
+
       age = Math.floor(parseInt(age));
       if (Number.isNaN(age)) return onError("Age must be a positive number");
-      if (level === '') return onError("Must choose one level");
+
+      if (level === '') return onError("Please choose one level");
       level = level === "Beginner" ? 1 : level === "Intermediate" ? 2 : 3;
-      if (place === '') return onError("Must input some destination");
+
+      if (place === '') return onError("Please input some destination");
       place = place.split(",").map(place => place.trim()).filter(place => place !== "");
-      if (date.every(day => day === false)) return onError("Must choose at least one free day");
+
+      if (date.every(day => day === false)) return onError("Please choose at least one free day");
+
       // inputs have passed validation
       await db.collection("users").doc(uid).set({
         isProfileCompleted: true,
+        avatar: avatarUrl,
         age: age,
         level: level,
         // intro: intro,
         // hobby: hobby,
         place: place,
-        date: date
+        date: date,
       }, {merge: true});
       // finish setting up profile
       return onSuccess(user);
-    }
-    else {
+    } else {
       return onError("No user found!");
     }
-
+  } catch (error) {
+    return onError(error);
+  }
 };
--- app/src/screen/SetupScreen.js
+++ app/src/screen/SetupScreen.js
@@ -1,8 +1,9 @@
 import React, {useState, useContext}  from 'react';
 import { StyleSheet,Text, Image, View, ScrollView} from 'react-native'
-import {Button, RadioButton, TextInput, Chip } from "react-native-paper";
+import {Button, RadioButton, TextInput, Chip, Avatar } from "react-native-paper";
 import { CommonActions } from "@react-navigation/native";
 import { showMessage } from "react-native-flash-message";
+import * as ImagePicker from "expo-image-picker";
 
 import Screen from "../component/screen"
 import * as Setup from "../../api/setup";
@@ -18,24 +19,23 @@ export default ({navigation}) => {
   // const [hobby, setHobby] = useState("");
   const [place, setPlace] = useState("");
   const [date, setDate] = useState([false, false, false, false, false, false, false]);
-  // const [mon, setMon] = useState(false);
   const daysInWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
   const userAPI = useContext(UserContext);
 
-  const handleSetUpProfile = () => {
-    Setup.setUpProfile(
-      {age, level, place, date},
+  // callback function when user presses 'Finish' to set up their profile
+  const handleSetUpProfile = async () => {
+     Setup.setUpProfile({age, level, place, date, avatar},
       () => {
         console.log("Success")
-        console.log(userAPI.user)
+        // console.log(userAPI.user)
         userAPI.changeUserState();
-        setTimeout(() => console.log(userAPI.user), 5000);
+        // setTimeout(() => console.log(userAPI.user), 5000);
       },
       (error) => {showMessage({
         message: error,
         type: "warning",
-        duration: 1500,
+        duration: 1600,
         floating: true,
         icon: "auto"
       });
@@ -43,107 +43,132 @@ export default ({navigation}) => {
     )
   };
 
+  // state & callback function to handle user's avatar picking
+  const [avatar, setAvatar] = useState(null);
+  let hasCameraRollPermission = false;
+  const handlePickImage = async () => {
+    if (!hasCameraRollPermission) {
+      let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (!permissionResult.granted) {
+        alert('Please enable access permission for camera roll!');
+        return;
+      }
+      else hasCameraRollPermission = true;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (!pickerResult.cancelled) {
+      console.log(pickerResult);
+      setAvatar({ uri: pickerResult.uri });
+      console.log(`avatar is null? ${avatar == null}`);
+    }
+  };
+
   return (
     <Screen style={styles.container} >
-      <ScrollView>
-      
-      <TextInput
-        label="Age"
-        placeholder="21"
-        keyboardType="numeric"
-        mode="contained"
-        value={age}
-        onChangeText={setAge}
-        style={{marginBottom:10, width:"95%"}}
-        // left={<TextInput.Icon name="email"/>}
+      <ScrollView style={{paddingHorizontal: 15}}>
+
+        <Text style={styles.title}>Set up your Profile</Text>
+        <View style={{alignItems: "center"}}>
+          <Avatar.Image
+            size={105}
+            source={
+              avatar !== null
+                ? ({ uri: avatar.uri })
+                : require('../../assets/ava1.jpg')
+            }
+            style={{marginTop: 10}}
+          />
+          <Button icon="camera" mode="contained" onPress={handlePickImage} style={styles.button}>
+            Add profile picture
+          </Button>
+        </View>
+
+        <TextInput
+          label="Age"
+          placeholder="25"
+          keyboardType="numeric"
+          mode="contained"
+          value={age}
+          onChangeText={setAge}
+          style={{marginBottom:10, width:"95%"}}
+          // left={<TextInput.Icon name="email"/>}
         />
 
-      <RadioButton.Item
-        label="Beginner"
-        value="Beginner"
-        status={ level === 'Beginner' ? 'checked' : 'unchecked' }
-        onPress={() => setLevel('Beginner')}
-      />
-      <RadioButton.Item
-        label="Intermediate"
-        value="Intermediate"
-        status={ level === 'Intermediate' ? 'checked' : 'unchecked' }
-        onPress={() => setLevel('Intermediate')}
-      />
-      <RadioButton.Item
-        label="Advanced"
-        value="Advanced"
-        status={ level === 'Advanced' ? 'checked' : 'unchecked' }
-        onPress={() => setLevel('Advanced')}
-      />
+        <RadioButton.Item
+          label="Beginner"
+          value="Beginner"
+          status={ level === 'Beginner' ? 'checked' : 'unchecked' }
+          onPress={() => setLevel('Beginner')}
+        />
+        <RadioButton.Item
+          label="Intermediate"
+          value="Intermediate"
+          status={ level === 'Intermediate' ? 'checked' : 'unchecked' }
+          onPress={() => setLevel('Intermediate')}
+        />
+        <RadioButton.Item
+          label="Advanced"
+          value="Advanced"
+          status={ level === 'Advanced' ? 'checked' : 'unchecked' }
+          onPress={() => setLevel('Advanced')}
+        />
 
-      {/*<TextInput*/}
-      {/*  label="Intro"*/}
-      {/*  placeholder="A few words to describe yourself..."*/}
-      {/*  keyboardType="default"*/}
-      {/*  mode="contained"*/}
-      {/*  value={intro}*/}
-      {/*  onChangeText={setIntro}*/}
-      {/*  style={{marginBottom:10, width:"95%"}}*/}
-      {/*  // left={<TextInput.Icon name="email"/>}*/}
-      {/*/>*/}
+        {/*<TextInput*/}
+        {/*  label="Intro"*/}
+        {/*  placeholder="A few words to describe yourself..."*/}
+        {/*  keyboardType="default"*/}
+        {/*  mode="contained"*/}
+        {/*  value={intro}*/}
+        {/*  onChangeText={setIntro}*/}
+        {/*  style={{marginBottom:10, width:"95%"}}*/}
+        {/*  // left={<TextInput.Icon name="email"/>}*/}
+        {/*/>*/}
 
-      <TextInput
-        label="Place"
-        placeholder="MacRitchie Reservoir, ..."
-        mode="contained"
-        value={place}
-        onChangeText={setPlace}
-        style={{marginBottom:10, width:"95%"}}
-        // left={<TextInput.Icon name="email"/>}
-      />
+        <TextInput
+          label="Place"
+          placeholder="MacRitchie Reservoir, ..."
+          mode="contained"
+          value={place}
+          onChangeText={setPlace}
+          style={{marginBottom:10, width:"95%"}}
+          // left={<TextInput.Icon name="email"/>}
+        />
 
-      {/*  Pick dates */}
-        {daysInWeek.map((item, index) => {
-            return (
-              <View style={{
-                margin: 5,
-                flexWrap: 'wrap',
-              }}>
-                <Chip
-                  key={index}
-                  mode="outlined" // changing display mode, default is flat
-                  height={30} //give desirable height to chip
-                  // textStyle={{ color:'white', fontSize: 15 }} //label properties
-                  // style={{ backgroundColor: "blue" }}
-                  selected={date[index]}
-                  selectedColor="blue"
-                  onPress={() => {
-                    let newDate = [...date];
-                    newDate[index] = !newDate[index];
-                    setDate(newDate);
-                    console.log('Clicked chip '+ item)
-                  }}
-                >
-                  { item }
-                </Chip>
-              </View>
-            );
-          })
-        }
+        {/*  Pick dates */}
+          {daysInWeek.map((item, index) => {
+              return (
+                <View style={{
+                  margin: 5,
+                  flexWrap: 'wrap',
+                }} key={index}>
+                  <Chip
+                    key={index}
+                    mode="outlined" // changing display mode, default is flat
+                    height={30} // give desirable height to chip
+                    textStyle={{ color:'black', fontSize: 15 }} //label properties
+                    // style={{ backgroundColor: "gray" }}
+                    selected={date[index]}
+                    selectedColor="green"
+                    onPress={() => {
+                      let newDate = [...date];
+                      newDate[index] = !newDate[index];
+                      setDate(newDate);
+                      console.log('Clicked chip '+ item)
+                    }}
+                  >
+                    { item }
+                  </Chip>
+                </View>
+              );
+            })
+          }
 
-      <Button
-        mode="contained"
-        // onPress={ () => navigation.dispatch(CommonActions.reset({
-        //     index: 0,
-        //     routes: [{
-        //       name: "Home",
-        //       // params: { name: user.displayName }
-        //       }]
-        //     })
-        //     )
-        // }
-        onPress={handleSetUpProfile}
-        style={styles.button}
-      >Finish
-      </Button>
+          <Button mode="contained" onPress={handleSetUpProfile} style={styles.button}>
+            Finish
+          </Button>
 
-        </ScrollView>
+      </ScrollView>
     </Screen>
   )
 }
@@ -157,10 +182,9 @@ const styles = StyleSheet.create({
   button: {
     // width:"70%",
     // borderRadius:25,
-    height:50,
     alignItems:"center",
     justifyContent:"center",
-    marginTop:10,
+    marginVertical:10
   },
   container: {
     flex: 1,
