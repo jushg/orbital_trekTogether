edeaa52beb6b9fae1a4098b0b3fd3415da8d315c
jushg <e0550425@u.nus.edu>
2021-06-26 02:52:55 +0800

    Move unnecessary stuff into one folder

--- app/src/feature/auth.js
+++ app/src/feature/auth.js
@@ -1,44 +1,6 @@
-// import { createSlice } from '@reduxjs/toolkit'
-// import { createSelector } from 'reselect'
-
-// const authSlice = createSlice({
-//     name: 'auth',
-//     initialState: {
-//         isLoading: true,
-//         isSignout: false,
-//         user: null,
-//     },
-//     reducers:{
-//         signIn: (state,{ payload }) => {
-//             state.isSignout = true
-//             state.user = payload
-//         },
-//         signOut: state => {
-//             state.isSignout = false
-//             state.userID = null
-//         },
-//         // restoredToken: (state,{ payload }) => {
-//         //     state.isLoading = false
-//         //     state.userID = payload.uid
-//         // }
-
-//     }
-// })
-
-// const { signIn, signOut } = authSlice.actions
-
-// export const signedIn = (user) => signIn({user})
-// export const signedOut = () => signOut()
-// // export const restoreToken = (user) => restoredToken({user})
-
-// export const getUserID = createSelector(
-//     (state) => state.auth,
-//     (auth) => auth.userID
-// )
-
-// export default authSlice.reducer
 
 import { createContext } from "react";
+
 export const UserContext = createContext();
 
 
